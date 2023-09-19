68f36119230c26e54c7400a50c4b840ffbb2feea
cafrmen <cafrmen@gmail.com>
2023-09-18 20:12:20 -0600

    Semantics and indentation

--- src/App.js
+++ src/App.js
@@ -4,151 +4,139 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[name, setName] = useState({value: '',
-                                  isTouched: false});
-  const[email, setEmail] = useState({value: '',
-                                    isTouched: false});
-  const[confirmEmail, setConfirmEmail] = useState({value: '',
-                                                  isTouched: false});
-  const[password,setPassword] = useState({value: '',
-                                        isTouched: false});
+	const[name, setName] = useState({value: '',
+									isTouched: false});
+	const[email, setEmail] = useState({value: '',
+										isTouched: false});
+	const[confirmEmail, setConfirmEmail] = useState({value: '',
+													isTouched: false});
+	const[password,setPassword] = useState({value: '',
+											isTouched: false});
 
-  //validate name
-  const nameErrorMessage = () => {
-    let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
-    let result = userCheck.test(name.value);
-    return !result ?
-        setName({value: '', isTouched: false}) :
-        null;
-  }
+	//validate name
+	const nameErrorMessage = () => {
+		let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+		let result = userCheck.test(name.value);
+		return !result ?
+			setName({value: '', isTouched: false}) :
+       		 null;
+	}
 
-  //validate email
-  const emailErrorMessage = () => {
-    setEmail({value: '', isTouched: false});
-    setConfirmEmail({value: '', isTouched: false});
-}
+	//validate email
+	const emailErrorMessage = () => {
+		setEmail({value: '', isTouched: false});
+		setConfirmEmail({value: '', isTouched: false});
+	}
 
-  //validate password
-  const passwordErrorMessage = () => {
-    setPassword({value: '', isTouched: false});
-  }
+	//validate password
+	const passwordErrorMessage = () => {
+		setPassword({value: '', isTouched: false});
+    }
 
   //validate form
-  const formValid = () => {
-    return (
-      name.value &&
-      email.value &&
-      confirmEmail.value &&
-      password.value.length > 7
-    )
-  }
+	const formValid = () => {
+		return (
+			name.value &&
+			email.value &&
+			confirmEmail.value &&
+			password.value.length > 7
+		)
+    }
 
-  //continue with the form
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('Great, you finished the first part!')
-  }
+	//continue with the form
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		alert('Great, you finished the first part!')
+ 	}
 
-  return (
-    <div className="App">
-      <h1>Client - Side Validating Form</h1>
-      <h6>* Please note that this form is designed to erase every input if your info is incorrect. The BUTTON will be disabled until you type the correct information!</h6>
-      <h3>Part 1 of 3</h3>
-      {/* The first part of this form */}
-      <form className='form' onSubmit={handleSubmit} >
-        {/* username */}
-        <label htmlFor="name">
-          Userame:
-        </label>
-        <input id="name"
-                type="text"
-                value={name.value}
-                placeholder='Type your name'
-                onChange={(e) => setName({...name, value: e.target.value})}
-                onBlur={() => setName({...name, isTouched: true})}
-                required />
-        {name.isTouched ?
-          nameErrorMessage() :
-          null}
-        <p>
-          * It must be alpha-numeric. Numbers have to be at the end. There can be zero or more of them at the end. Cannot start with a number. At least two characters long. A two characters username can only use alphabet letters.
-        </p>
+	return (
+		<div className="App">
+			<header>
+				<h1>Client - Side Validating Form</h1>
+				<h6>* Please note that this form is designed to erase every input if your info is incorrect. The BUTTON will be disabled until you type the correct information!</h6>
+				<h3>Part 1 of 3</h3>
+			</header>
 
-        {/* Email */}
-        <hr />
-        <label htmlFor='email'>
-          Email:
-        </label>
-        <input id='email'
-                type='email'
-                value={email.value}
-                placeholder='Type your email'
-                onChange={(e) => setEmail({...email, value: e.target.value})}
-                onBlur={() => setEmail({...email, isTouched: true})}
-                required />
+			{/* The first part of this form */}
+			<main>
+				<form className='form' onSubmit={handleSubmit} >
+					{/* username */}
+					<label htmlFor="name">
+						Userame:
+					</label>
+					<input id="name"
+							type="text"
+							value={name.value}
+							placeholder='Type your name'
+							onChange={(e) => setName({...name, value: e.target.value})}
+							onBlur={() => setName({...name, isTouched: true})}
+							required />
+					{name.isTouched ?
+						nameErrorMessage() :
+						null}
+					<p>
+						* It must be alpha-numeric. Numbers have to be at the end. There can be zero or more of them at the end. Cannot start with a number. At least two characters long. A two characters username can only use alphabet letters.
+					</p>
+					<hr />
 
-        {/* Confirm email */}
-        <hr />
-        <label htmlFor='confirmEmail' >
-          Confirm your email:
-        </label>
-        <input id='confirmEmail'
-                type='email'
-                value={confirmEmail.value}
-                placeholder='Type your email ... again'
-                onChange={(e) => setConfirmEmail({...confirmEmail, value: e.target.value})}
-                onBlur={() => setConfirmEmail({...confirmEmail, isTouched: true})}
-                required />
-        {email.isTouched && confirmEmail.isTouched && email.value !== confirmEmail.value ?
-              emailErrorMessage() :
-              null}
-        <p>
-            * Please notice that the emails must be the same.
-        </p>
+					{/* Email */}
+					<label htmlFor='email'>
+						Email:
+					</label>
+					<input id='email'
+							type='email'
+							value={email.value}
+							placeholder='Type your email'
+							onChange={(e) => setEmail({...email, value: e.target.value})}
+							onBlur={() => setEmail({...email, isTouched: true})}
+							required />
+					<hr />
 
-        {/* Password */}
-        <hr />
-        <label htmlFor='password' >
-          Password:
-        </label>
-        <input id='password'
-                type='password'
-                value={password.value}
-                placeholder="Type your password"
-                onChange={(e) => setPassword({...password, value: e.target.value})}
-                onBlur={() => setPassword({...password, isTouched: true})}
-                required />
-        {password.isTouched && password.value.length < 8 ?
-                  passwordErrorMessage() :
-                  null}
-        <p>
-          * Minimum 8 characters.
-        </p>
-      </form>
+					{/* Confirm email */}
+					<label htmlFor='confirmEmail' >
+						Confirm your email:
+					</label>
+					<input id='confirmEmail'
+							type='email'
+							value={confirmEmail.value}
+							placeholder='Type your email ... again'
+							onChange={(e) => setConfirmEmail({...confirmEmail, value: e.target.value})}
+							onBlur={() => setConfirmEmail({...confirmEmail, isTouched: true})}
+							required />
+					{email.isTouched && confirmEmail.isTouched && email.value !== confirmEmail.value ?
+						emailErrorMessage() :
+						null}
+					<p>
+						* Please notice that the emails must be the same.
+					</p>
+					<hr />
 
-      {/* Conditional rendering in order to continue with the form */}
-      {!formValid() ?
-      null :
-      <MiddleForm />}
-    </div>
-  );
+					{/* Password */}
+					<label htmlFor='password' >
+						Password:
+					</label>
+					<input id='password'
+							type='password'
+							value={password.value}
+							placeholder="Type your password"
+							onChange={(e) => setPassword({...password, value: e.target.value})}
+							onBlur={() => setPassword({...password, isTouched: true})}
+							required />
+					{password.isTouched && password.value.length < 8 ?
+						passwordErrorMessage() :
+						null}
+					<p>
+						* Minimum 8 characters.
+					</p>
+				</form>
+			</main>
+
+			{/* Conditional rendering in order to continue with the form */}
+			{!formValid() ?
+				null :
+				<MiddleForm />}
+		</div>
+    );
 }
 
 export default App;
-
-/*
-HACER LO DEL PASSWORD
-
-name
-email
-confirm email
-password
-
-age range - select
-us number
-are you a dev - radio
-
-rate my form - range
-comments
-submit
-*/
--- src/components/MiddleForm.js
+++ src/components/MiddleForm.js
@@ -4,129 +4,118 @@ import FinalForm from './FinalForm';
 import '../App.css';
 
 function MiddleForm() {
-  const[age, setAge] = useState('age');
-  const[phone, setPhone] = useState({value: '',
-                                    isTouched: false});
-  const[dev, setDev] = useState('');
+	const[age, setAge] = useState('age');
+	const[phone, setPhone] = useState({value: '',
+										isTouched: false});
+	const[dev, setDev] = useState('');
 
-  // validate age
-  const errorAgeMessage = () => {
-    setAge('age');
-  }
+	// validate age
+	const errorAgeMessage = () => {
+		setAge('age');
+	}
 
-  //validate US phone number
-  const phoneErrorMessage = () => {
-      let phoneOneRegex = /^1?[- ]?\((\d{3})\)[- ]?(\d{3})[- ]?(\d{4})$/;
-      let phoneTwoRegex = /^1?[- ]?(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
+	//validate US phone number
+	const phoneErrorMessage = () => {
+		let phoneOneRegex = /^1?[- ]?\((\d{3})\)[- ]?(\d{3})[- ]?(\d{4})$/;
+		let phoneTwoRegex = /^1?[- ]?(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
 
-      return phoneOneRegex.test(phone.value) || phoneTwoRegex.test(phone.value) ?
-              null :
-              setPhone({value: '', isTouched: false});
-  }
+		return phoneOneRegex.test(phone.value) || phoneTwoRegex.test(phone.value) ?
+				null :
+				setPhone({value: '', isTouched: false});
+	}
 
-  //validate form
-  const formValid = () => {
-    return (
-      age === 'good' &&
-      phone.value &&
-      dev.length > 1
-    )
-  }
+	//validate form
+	const formValid = () => {
+		return (
+			age === 'good' &&
+			phone.value &&
+			dev.length > 1
+		)
+	}
 
-  //continue with the form
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('Just one more to finish!')
-  }
+	//continue with the form
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		alert('Just one more to finish!')
+	}
 
-  return (
-    <div>
-      <h3>Part 2 of 3</h3>
-      <form className='form' onSubmit={handleSubmit} >
-        {/* Age selecctor */}
-        <label htmlFor='age' >
-          Select your range age:
-        </label>
-        <select id='age'
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-                required >
-          <option value='age'>age</option>
-          <option value='kiddo' >Kiddo under 18</option>
-          <option value='good'>18 or more</option>
-        </select>
-        {age === 'kiddo' ?
-            errorAgeMessage() :
-            null }
-        <p>
-          * You must be at least 18 years old.
-        </p>
+	return (
+		<div>
+			<header>
+				<h3>Part 2 of 3</h3>
+			</header>
 
-        {/* Phone input */}
-        <hr />
-        <label htmlFor='phone' >
-          Phone number:
-        </label>
-        <input id='phone'
-                type='text'
-                value={phone.value}
-                placeholder='Type your US phone number'
-                onChange={(e) => setPhone({...phone, value: e.target.value})}
-                onBlur={() => setPhone({...phone, isTouched: true})}
-                required />
-        {phone.isTouched && phone.value.length > 0 ?
-                  phoneErrorMessage() :
-                  null}
-        <p>
-          * This is intentionally a 'text' type input. You must write a valid US phone number such as: 555-555-5555, (555)555-5555, (555) 555-5555, 555 555 5555, 5555555555, 1 555 555 5555, 1 555-555-5555, 1 (555) 555-5555, 1(555)555-5555...
-        </p>
+			<main>
+				<form className='form' onSubmit={handleSubmit}>
+					{/* Age selecctor */}
+					<label htmlFor='age' >
+						Select your range age:
+					</label>
+					<select id='age'
+							value={age}
+							onChange={(e) => setAge(e.target.value)}
+							required >
+						<option value='age'>age</option>
+						<option value='kiddo' >Kiddo under 18</option>
+						<option value='good'>18 or more</option>
+					</select>
+					{age === 'kiddo' ?
+						errorAgeMessage() :
+						null }
+					<p>
+						* You must be at least 18 years old.
+					</p>
+					<hr />
 
-        {/* Yes or no input */}
-        <hr />
-        <label>Are you a dev?</label>
-        <input className='radio' type="radio"
-                name="dev"
-                value="yes"
-                id="yes"
-                checked={dev === 'yes'}
-                onChange={(e) => setDev(e.target.value)} />
-        <label className='inputText' htmlFor="yes">Yes</label>
+					{/* Phone input */}
+					<label htmlFor='phone' >
+						Phone number:
+					</label>
+					<input id='phone'
+							type='text'
+							value={phone.value}
+							placeholder='Type your US phone number'
+							onChange={(e) => setPhone({...phone, value: e.target.value})}
+							onBlur={() => setPhone({...phone, isTouched: true})}
+							required />
+					{phone.isTouched && phone.value.length > 0 ?
+						phoneErrorMessage() :
+						null}
+					<p>
+						* This is intentionally a 'text' type input. You must write a valid US phone number such as: 555-555-5555, (555)555-5555, (555) 555-5555, 555 555 5555, 5555555555, 1 555 555 5555, 1 555-555-5555, 1 (555) 555-5555, 1(555)555-5555...
+					</p>
+					<hr />
 
-        <input className='radio' type="radio"
-                name="dev"
-                value="no"
-                id="no"
-                checked={dev === 'no'}
-                onChange={(e) => setDev(e.target.value)} />
-        <label className='inputText' htmlFor="no">No</label>
+					{/* Yes or no input */}
+					<label>Are you a dev?</label>
+					<input className='radio' type="radio"
+							name="dev"
+							value="yes"
+							id="yes"
+							checked={dev === 'yes'}
+							onChange={(e) => setDev(e.target.value)} />
+					<label className='inputText' htmlFor="yes">Yes</label>
 
-        <p>* Please select one option.</p>
-      </form>
+					<input className='radio' type="radio"
+							name="dev"
+							value="no"
+							id="no"
+							checked={dev === 'no'}
+							onChange={(e) => setDev(e.target.value)} />
+					<label className='inputText' htmlFor="no">No</label>
 
+					<p>
+						* Please select one option.
+					</p>
+				</form>
 
-        {/* Conditional rendering in order to continue with the form */}
-        {!formValid() ?
-        null :
-        <FinalForm />}
-    </div>
+				{/* Conditional rendering in order to continue with the form */}
+				{!formValid() ?
+					null :
+					<FinalForm />}
+			</main>
+		</div>
   )
 }
 
 export default MiddleForm;
-
-/*
-HACER LO DEL PASSWORD
-
-name
-email
-confirm email
-password
-
-age range - select
-us number
-are you a dev - radio
-
-rate my form - range
-comments
-submit
-*/
